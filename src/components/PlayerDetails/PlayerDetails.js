b709ff9d7c74a2f284da1905b0ac639e3b3fc67a
srafsan <srafsan98@tutanota.com>
2022-01-18 22:40:00 +0600

    Rating Added in the Player Details

--- src/components/PlayerDetails/PlayerDetails.js
+++ src/components/PlayerDetails/PlayerDetails.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Rating from 'react-rating';
+import './PlayerDetails.css'
 
 const Details = () => {
     const { playerDetails } = useParams();
@@ -25,10 +26,17 @@ const Details = () => {
                         <img className='w-75' src={player[0]?.image} alt="" />
                     </div>
                     <div className="col-md-5 shadow p-3 mb-5 bg-body rounded">
-                        <h5>Details {player[0]?.id}</h5>
+                        <h5>Player# {player[0]?.id}</h5>
                         <h2>Name: {player[0]?.name}</h2>
                         <h3>{player[0]?.profession} Player</h3>
-                        <Rating initialRating={player[0]?.rating} emptySymbol="far fa-star icon-color" fullSymbol="fas fa-star icon-color" readonly></Rating>
+                        {/* <Rating initialRating={player[0]?.rating} emptySymbol="far fa-star icon-color" fullSymbol="fas fa-star icon-color" readonly></Rating> */}
+
+                        <Rating
+                            initialRating={player[0]?.rating}
+                            emptySymbol="far fa-star icon-color"
+                            fullSymbol="fas fa-star icon-color"
+                            readonly
+                        ></Rating>
                         <p>{player[0]?.description}</p>
                     </div>
                 </div>
